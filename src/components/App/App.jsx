e395f84019d5225684316c78b7b181052b808457
AdrianSiwek <105585774+AdrianSiwek@users.noreply.github.com>
2023-03-21 23:41:10 +0100

    update

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,16 +1,23 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+import styles from './App.module.css';
+import ContactForm from '../ContactForm/ContactForm'
+
+export class App extends Component {
+  state = {
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    filter: '',
+    } 
+  render() { 
+    return (
+      <div>
+        <ContactForm/>
+      </div>
+    );
+  }
+}
+ 
--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,3 +1,48 @@
 import { Component } from 'react';
 import PropTypes from 'prop-types';
 import style from './ContactForm.module.css';
+
+
+class ContactForm extends Component {
+    state = {  } 
+    render() { 
+        return (
+            <div>
+                <form className={style.submit}>
+                    <label className={style.label}>
+                        Name
+                        <input
+                            type="text"
+                            name="name"
+                            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+                            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+                            required
+                        />
+                    </label>
+                    <label className={style.label}>
+                    Number
+                        <input
+                            className={style.input}
+                            type="tel"
+                            name="number"
+                            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+                            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+                            value={this.state.number}
+                            onChange={this.handleChange}
+                            required
+                        />
+                    </label>
+                    <button
+                        className={style.btn}
+                        type="submit"
+                        disabled={!this.state.name && !this.state.number}
+                    >
+                        <span>Add contact</span> 
+                    </button>
+                </form>
+            </div>
+        );
+    }
+}
+ 
+export default ContactForm;
